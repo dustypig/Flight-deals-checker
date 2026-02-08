8c90aae368e88916e603630552ccb40b7fa4d980
dustypig <lousid@proton.me>
2026-02-08 15:38:15 +0800

    Update flight_data.py

--- flight_data.py
+++ flight_data.py
@@ -1,28 +1,22 @@
+import os
 import requests
 import datetime
 
 class FlightData:
-    def __init__(self):
-         pass
-
     def get_flight_prices(self, city, city_code, departure_city):
-        six_months = datetime.datetime.now() + datetime.timedelta(180)
-        six_months_time = six_months.date().strftime("%d/%m/%Y")
-        tomorrow = datetime.datetime.now() + datetime.timedelta(1)
-        tomorrow_date = tomorrow.date().strftime("%d/%m/%Y")
-        api_end_point = "https://tequila-api.kiwi.com/v2/search"
-        parameters = {
-            "fly_from": f"{departure_city}",
-            "fly_to": f"{city_code}",
-            "date_from": f"{tomorrow_date}",
-            "date_to": f"{six_months_time}"
+        tomorrow = (datetime.datetime.now() + datetime.timedelta(days=1)).strftime("%d/%m/%Y")
+        six_months = (datetime.datetime.now() + datetime.timedelta(days=180)).strftime("%d/%m/%Y")
+
+        url = "https://tequila-api.kiwi.com/v2/search"
+        headers = {
+            "apikey": os.getenv("KIWI_API_KEY")
         }
-        header = {
-            "apikey": "Iak0yghjoest46eSGnmoQaAJHFo5YNUN",
-            "Content-Type": "application/json"
+        params = {
+            "fly_from": departure_city,
+            "fly_to": city_code,
+            "date_from": tomorrow,
+            "date_to": six_months
         }
 
-        response = requests.get(url=api_end_point, params=parameters, headers=header)
-        data = response.json()["data"][0]["price"]
-        return data
-
+        response = requests.get(url, headers=headers, params=params)
+        return response.json()["data"][0]["price"]
